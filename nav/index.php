ec50991c37ed11ed4f6db1c750a7a1311c54e09c
SatriaReza <74123282+SatriaReza@users.noreply.github.com>
2022-10-16 16:01:38 +0700

    Update index.php
    Ngubah direct link di tulisan kasirku

--- nav/index.php
+++ nav/index.php
@@ -80,7 +80,7 @@ if (!isset($_SESSION['username'])){
   </svg>
   
     <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: 100vh;">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
         <span class="fs-4 fw-bold">KASIRKU</span>
       </a>
       <hr>
